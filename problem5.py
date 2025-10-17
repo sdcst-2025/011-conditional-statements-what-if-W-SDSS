03d670ccdf7a5a60295b1f0378368d7dc5a4d89c
W-SDSS <wakanai027@deltalearns.ca>
2025-10-17 16:37:53 -0700

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -29,3 +29,14 @@ Enter c: 8
 the equation can not be factored
 
 """
+
+a = float(input("Enter a: ")
+b = float(input("Enter b: ")
+c = float(input("Enter c: ")
+
+D = b**2 - 4*a*c
+
+if D < 0:
+  print("the equation can not be factored")
+else:
+  print("the equation can be factored")
